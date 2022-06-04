e9f135c1d5f43bf008d1af107672caed2fe5150d
Terrance <terrancekanyezi@gmail.com>
2022-06-04 16:28:47 -0700

    Created file for challenge 5. printf()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/*
+ * C program that prints exactly
+ * "Programming is like building a multilingual puzzle
+ */
+
+#include<stdio.h>
+
+int main(void)
+{
+	char myString[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(myString);
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/*
+ * C program that prints exactly:
+ * "with proper grammar, but the outcome is a piece of art,"
+ * Using printf()
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
