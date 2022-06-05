5a3bbb1add1c23f78f975b3834e8e32342f6be26
Terrance <terrancekanyezi@gmail.com>
2022-06-04 18:43:30 -0700

    Added files 100-intel and 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+/*
+ * C program that prints exactly
+ * "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ * Without printf, puts or related functions.
+ */
+
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - execution begins here
+ *
+ * Return: 1 to indicate error (printing to stderr)
+ */
+int main(void)
+{
+	char myString[] = "and that piece of art is useful\" "
+		"- Dora Korpar, 2015-10-19";
+
+	fwrite(myString, sizeof(char), strlen(myString), stderr);
+	return (1);
+}
