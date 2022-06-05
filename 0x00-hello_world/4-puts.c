b93523a13e5bce9d975db6a2a50d032362583911
Terrance <terrancekanyezi@gmail.com>
2022-06-04 17:27:05 -0700

    Corrections: 4-puts.c, 5-printf.c, 6-size.c
    Added commen on main() to mtch betty-doc expectations.
    
     On branch master
     Changes to be committed:
    	modified:   4-puts.c
    	modified:   5-printf.c
    	modified:   6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 /*
  * C program that prints exactly
  * "Programming is like building a multilingual puzzle
- */
+*/
 
 #include<stdio.h>
 
+/**
+ * main - execution begins here
+ *
+ * Return: 0 on successful execution
+ */
 int main(void)
 {
 	char myString[] = "\"Programming is like building a multilingual puzzle";
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,13 @@
  * Using printf()
  */
 
+#include<stdio.h>
+
+/**
+ * main - execution begins here
+ *
+ * Return: 0 on successful execution
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
