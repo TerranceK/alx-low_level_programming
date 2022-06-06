f4f85d44a066c37a49fc5770936515e3537b0291
Terrance <terrancekanyezi@gmail.com>
2022-06-06 15:07:16 -0700

    Added file solution for question 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+/*
+ * program that prints all possible different combinations of two digits.
+ */
+#include<stdio.h>
+
+/**
+ * main - execution begins here
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	int x = 0;
+	int y = 0;
+	int i = 0;
+	int j = 0;
+	int small, large;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i != j)
+			{
+				small = (i * 10) + j;
+				large = (j * 10) + i;
+				if (small < large)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
