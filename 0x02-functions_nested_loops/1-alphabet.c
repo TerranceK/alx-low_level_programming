b84f9082ea7a852121c2a05c94e0f17f566c87f6
Terrance <terrancekanyezi@gmail.com>
2022-06-11 05:41:14 -0700

    Changed 1-alphabet.c to make it Betty compliant

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,11 @@
 /**
  * print_alphabet - print all letters of the alphabet in lowercase, followed
  * by a newline.
- *
- * @i: Counter from 97 to 122
-  */
+ */
 void print_alphabet(void)
 {
 	int i = 97;
+
 	while (i <= 122)
 	{
 		_putchar(i);
