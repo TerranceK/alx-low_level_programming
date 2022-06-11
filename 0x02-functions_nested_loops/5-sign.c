40ea282e3b229dfa90f9da06008a5d9cafdeab09
Terrance <terrancekanyezi@gmail.com>
2022-06-11 08:58:44 -0700

    Added file 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+/**
+ * print_sign - Prints the sign of a given number.
+ *
+ * @n: Input value to be tested.
+ * Return: 1 if n > 0, 0 if n == 0 and -1 if n <0.
+ */
+
+int print_sign(int n)
+{
+	int nsign;
+
+	if (n > 0)
+	{
+		nsign = 1;
+		_putchar('+');
+	}
+	else if (n < 0)
+	{
+		nsign = 0;
+		_putchar('-');
+	}
+	else
+	{
+		lcase = 0;
+		_putchar('0');
+	}
+
+	return (lcase);
+}
+
