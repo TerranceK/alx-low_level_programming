cc2bde9ba62ca567745a78c123b90bfa3ae43e46
Terrance <terrancekanyezi@gmail.com>
2022-06-11 13:14:33 -0700

    Fixed semantic bugs that prevented the time to be printed correctly, and added a comdition to break the loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,18 +9,25 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j < 4; j++)
+		for (j = 0; j < 10; j++)
 		{
 			for (k = 0; k < 6; k++)
 			{
 				for (l = 0; l < 10; l++)
 				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
+					if ((i == 2) && (j == 4) && (k == 0) && (l == 0))
+					{
+						break;
+					}
+					else
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar('\n');
+					}
 				}
 			}
 		}
