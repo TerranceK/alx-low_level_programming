f45879445e2142e4f1ec8c334e3f0d26427edc1d
Terrance <terrancekanyezi@gmail.com>
2022-06-11 09:22:16 -0700

    Created new file - 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * _abs - Returns absolute value of a given integer.
+ *
+ * @n: Input value to be "absoluted".
+ * Return: absolute value (int).
+ */
+
+int _abs(int n)
+{
+
+	if (n > 0)
+	{
+		n = n;
+	}
+	else
+	{
+		n *= -1;
+	}
+
+	return (n);
+}
+
